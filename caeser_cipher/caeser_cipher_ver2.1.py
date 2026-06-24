9dd32f4d802534cd342e3e49bd275be54008ca8f
Railerival <raiabhimanyu09@gmail.com>
2026-06-24 09:26:29 +0530

    fixed the file names & fixed the caeser ver2.1

--- caeser_cipher/caeser_cipher_ver2.1.py
+++ caeser_cipher/caeser_cipher_ver2.1.py
@@ -1,6 +1,5 @@
 import string
 
-c = 0
 lower_case = string.ascii_lowercase
 upper_case = string.ascii_uppercase
 
@@ -10,22 +9,19 @@ if key < 0 or key > 25:
     print("Error the key is too high")
     quit()
 if x == "e":
-    c = 1
     msg = input("Enter the message to encrypt:\n")
+    shifted = lower_case[key:] + lower_case[:key]
+    cipher_ascii_code = msg.maketrans(lower_case, shifted)
+    print(msg.translate(cipher_ascii_code))
 elif x == 'd':
-    c = 2
     code = input("Enter the message to decrypt:\n")
+    shifted = lower_case[(26 - (key + 1)):]+lower_case[:26 - (key + 1)]
+    dec_msg_ascii_code = code.maketrans(shifted,lower_case)
+    print(code.translate(dec_msg_ascii_code))
 else:
     print("that is not a valid command")
 
-if c == 1:
-    shifted = string.ascii_lowercase[key:] + string.ascii_lowercase[:key]
-    cipher_ascii_code = msg.maketrans(string.ascii_lowercase, shifted)
-    print(msg.translate(cipher_ascii_code))
-if c == 2:
-    shifted = string.ascii_lowercase[(26 - (key + 1)):]+string.ascii_lowercase[:26 - (key + 1)]
-    dec_msg_ascii_code = code.maketrans(shifted,string.ascii_lowercase)
-    print(code.translate(dec_msg_ascii_code))
+    
 
 
 
