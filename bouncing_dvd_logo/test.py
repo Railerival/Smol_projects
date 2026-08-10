9925a80750d48cc934ae82759914028a962aa264
Railerival <raiabhimanyu09@gmail.com>
2026-08-10 16:19:45 +0530

    deleted older versions

--- bouncing_dvd_logo/test.py
+++
@@ -1,8 +0,0 @@
-string = "hi"
-while True:
-  try:
-    print(string + 3)
-    break
-  except TypeError:
-    print("except block")
-print("hi")
--- caeser_cipher/test.py
+++
@@ -1,8 +0,0 @@
-# how mutables and immutables are different in usage of methods
-string = "hi how are u"
-print(string.capitalize)
-string.capitalize()
-print(string)
-print(string.capitalize())
-hh = string.capitalize()
-print(hh)
